78716c4228695972ca0991e20ffe038d756cdc18
trakuta <medvepuh@gmail.com>
2023-04-22 13:23:25 +0300

    добавлен горизантальный угол

--- ballista/utils.py
+++ ballista/utils.py
@@ -1,5 +1,26 @@
+import math
+
+
 def calc_dist(p1, p2):
     return sum([(p1[i] - p2[i]) ** 2 for i in range(len(p1))]) ** 0.5
+
+
+def horizontal_angl(canon: tuple, aim: tuple):
+    if canon[0] == aim[0]:
+        return 0
+    elif canon[0] > aim[0]:
+        a = canon[0] - aim[0]
+    elif canon[0] < aim[0]:
+        a = aim[0] - canon[0]
+    b = math.fabs(canon[1]) + aim[1]
+    angle_rad = math.atan(a/b)
+    angl_degr = math.degrees(angle_rad)
+
+    if canon[0] > aim[0]:
+        return angl_degr
+    elif canon[0] < aim[0]:
+        return -angl_degr
+
 
 if __name__ == 'main':
     print(calc_dist((0, 0), (2, 1)))
